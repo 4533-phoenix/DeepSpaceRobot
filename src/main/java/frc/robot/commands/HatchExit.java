24644c7936976f6b139baeebd7109a4b112a51a1
Anirudh Valiveti <46406394+asvaliveti@users.noreply.github.com>
2019-02-27 16:57:22 -0500

    changed hatch down to addsequential instead of add parallel

--- src/main/java/frc/robot/commands/HatchExit.java
+++ src/main/java/frc/robot/commands/HatchExit.java
@@ -30,7 +30,7 @@ public class HatchExit extends CommandGroup {
     // e.g. if Command1 requires chassis, and Command2 requires arm,
     // a CommandGroup containing them would require both the chassis and the
     // arm.
+    this.addSequential(new ElevatorDownCommand());
     this.addParallel(new DrivePosition(-3));
-    this.addParallel(new ElevatorDownCommand());
   }
 }
